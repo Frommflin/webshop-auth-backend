7d2bb4f13147ab901544bbf907a3fafcb95189d4
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-10-09 22:31:15 +0200

    sereration of server and app

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import app from "./app.js";
 
-dotenv.config();
-
-const app = express();
-const PORT = 3000 || 5000;
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Auth backend is running");
-});
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
