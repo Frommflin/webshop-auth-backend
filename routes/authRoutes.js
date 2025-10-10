e5d7faebec6f574e350213b8880f1b7117a744d0
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-10-10 11:33:56 +0200

    register route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,4 +6,8 @@ router.post("/auth/login", (req, res) => {
   res.send("Login route");
 });
 
+router.post("/auth/register", (req, res) => {
+  res.send("Register route");
+});
+
 export default router;
