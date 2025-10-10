1faa424641d794135f408c12ffa41a424e7d24a4
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-10-10 11:41:05 +0200

    logout route, profile route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,4 +10,12 @@ router.post("/auth/register", (req, res) => {
   res.send("Register route");
 });
 
+router.post("/auth/logout", (req, res) => {
+  res.send("Logout route");
+});
+
+router.post("/auth/profile", (req, res) => {
+  res.send("Profile route");
+});
+
 export default router;
