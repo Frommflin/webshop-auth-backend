3c754af44b727a00799c91159289b741fef5de0b
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-17 23:38:52 +0200

    implementing usage of MongoMemoryServer as fallback to missing connectionsting in .env

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,21 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+
+let mongoServer;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+    let mongoConString = process.env.MONGODB_CONNECTION_STRING;
+
+    if (!mongoConString) {
+      console.log(
+        "No MongoDB connection string found in .env, using MongoMemoryServer for CI/test"
+      );
+      mongoServer = await MongoMemoryServer.create();
+      mongoConString = mongoServer.getUri();
+    }
+
+    const conn = await mongoose.connect(mongoConString);
     console.log(`MongoDB connected: ${conn.connection.name}`);
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
