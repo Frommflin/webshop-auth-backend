9ba9b82c72c758c3c053bf2c1881f1160b3e0b34
Klara Frommelin <freakyfrommelin@outlook.com>
2025-10-18 00:20:22 +0200

    adjusting server to prevent test-enviroment to get stuck at listening to port

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
