e5735bc332c43bb9486ba2e66c846b17ea438ee7
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-10-22 21:43:36 +0200

    hasning saltning for register

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,3 +1,6 @@
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
+
 const registerController = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -6,6 +9,13 @@ const registerController = async (req, res) => {
         .status(400)
         .json({ message: "Name, email, and password are required" });
     }
+
+    const saltrounds = parseInt(process.env.BCRYPT_SALT_ROUNDS || "12", 10);
+    const passwordHash = await bcrypt.hash(password, saltrounds);
+
+    const newUser = new User({ name, email, passwordHash });
+    await newUser.save();
+
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
